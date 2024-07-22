d0172886f8f292411cbc5587c956fc3ec7a80cae
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-22 15:41:09 -0400

    Create methods for company class

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,14 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    Freebie.create(item_name: item_name, value: value,
+                  dev: dev, company: self)
+  end
+
+  def self.oldest_company
+    oldest_founding_year = Company.minimum(:founding_year)
+    Company.where(founding_year: oldest_founding_year)
+  end
 end
