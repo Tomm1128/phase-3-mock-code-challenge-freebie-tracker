131d9d230eac399b5cd78532119f83cd279c5254
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-22 15:41:44 -0400

    Create methods for freebie class

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -3,6 +3,6 @@ class Freebie < ActiveRecord::Base
   belongs_to :company
 
   def print_details
-    "#{self.dev.name} owns a #{self.name} from #{self.company.name}"
+    "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
   end
 end
